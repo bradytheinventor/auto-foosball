90330b7e6722f211b90c3b884a8e20d1393655ff
bradytheinventor <bcra1234@gmail.com>
2017-10-21 09:35:38 -0500

    Created Motor class
    Created class to interface with stepper motors using wiringPi and the
    GPIO pins.

---
+++ src/Motor.cpp
@@ -0,0 +1,55 @@
+//Motor.cpp
+#include <wiringPi.h>
+#include "Motor.h"
+
+//load motor GPIO pins on object creation
+Motor::Motor(int A1, int A2, int B1, int B2) {
+	//set up wiringPi GPIO interface
+	wiringPiSetup();
+
+	//load GPIO pins
+	pins[0] = A1;
+	pins[1] = A2;
+	pins[2] = B1;
+	pins[3] = B2;
+
+	//set GPIO pins to output mode
+	pinMode(pins[0], OUTPUT);
+	pinMode(pins[1], OUTPUT);
+	pinMode(pins[2], OUTPUT);
+	pinMode(pins[3], OUTPUT);
+}
+
+//update motor GPIO pins to execute motor step
+void Motor::step(int direction) {
+	//write GPIO values to pins
+	for(int p = 0; p < 4; p++) {
+		digitalWrite(pins[p], seq[stepCounter][p]);
+	}
+
+	//update counter to track GPIO values
+	stepCounter += direction;
+
+	//apply bounds to counter
+	if(stepCounter > stepCount) {
+		stepCounter = 0;
+	} else if(stepCounter < 0) {
+		stepCounter = stepCount + direction;
+	}
+}
+
+//step motor CW by a given number of steps (looking at shaft)
+void Motor::forward(int interval, int steps) {
+	for(int i = 0; i < steps; i++) {
+		step(1);
+		delay(interval);
+	}
+}
+
+//step motor CCW by a given number of steps (looking at shaft)
+void Motor::backward(int interval, int steps) {
+	for(int i = 0; i < steps; i++) {
+		step(-1);
+		delay(interval);
+	}
+}
---
+++ src/Motor.h
@@ -0,0 +1,30 @@
+//Motor.h
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+class Motor {
+private:
+	//store the GPIO pin states needed to execute a particular movement
+	int stepCount = 8;
+	int stepCounter = 0;
+
+	int seq[8][4] = {
+			{1,0,0,1},
+			{1,0,0,0},
+			{1,1,0,0},
+			{0,1,0,0},
+			{0,1,1,0},
+			{0,0,1,0},
+			{0,0,1,1},
+			{0,0,0,1}
+	};
+	int pins[4];
+
+public:
+	Motor(int A1, int A2, int B1, int B2);
+	void step(int direction);
+	void forward(int interval, int steps);
+	void backward(int interval, int steps);
+};
+
+#endif
