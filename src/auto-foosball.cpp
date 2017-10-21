b0356d73274c34bab537730e777f26ea732da97c
bradytheinventor <bcra1234@gmail.com>
2017-10-21 09:36:25 -0500

    Removed Hello World, added Motor test loop
    Removed Hello World test code, replaced with a Motor test loop.

--- src/auto-foosball.cpp
+++ src/auto-foosball.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include "Motor.h"
 using namespace std;
 
-void setup();
-
 int main() {
-	setup();
-    cout << "Hello World!" << endl;
+	//create objects
+	Motor m = Motor(0, 1, 2, 3);
+
+	//main program loop
+	while(1) {
+		m.forward(10, 100);
+		m.backward(10, 100);
+	}
+
 	return 0;
-}
-
-void setup() {
-
 }
