c9550de248f75da6c2b3889f435d659dec2b6e05
DougZuercherEDU <32179661+DougZuercherEDU@users.noreply.github.com>
2017-10-21 14:01:55 -0500

    Added "Player.h" dependency, added player movement test.

--- src/auto-foosball.cpp
+++ src/auto-foosball.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include "Motor.h"
+#include "Player.h"
+
 using namespace std;
 
 int main() {
 	//create objects
 	Motor m = Motor(0, 1, 2, 3);
-
-	//main program loop
+    Player p = Player(0, 200, 0, 1, 2, 3);
+    
+    // Move player to 500mm location
+    p.moveTo(500);
+    
+    //main program loop
 	while(1) {
-		m.forward(10, 100);
-		m.backward(10, 100);
+        
+//        m.forward(10, 100);
+//        m.backward(10, 100);
 	}
-
 	return 0;
 }
