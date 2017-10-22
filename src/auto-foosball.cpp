f0764c5c4af86e6de3e3b8cfd29a1bc08af5846c
bradytheinventor <bcra1234@gmail.com>
2017-10-21 22:44:59 -0500

    Added Camera testing code to main loop
    Added code to test the Camera class to the main loop. Removed Motor test
    code.

--- src/auto-foosball.cpp
+++ src/auto-foosball.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #include "Motor.h"
+#include "Camera.h"
+
 using namespace std;
+using namespace cv;
 
 int main() {
 	//create objects
 	Motor m = Motor(0, 1, 2, 3);
-
+	Camera c = Camera(0);
+ 
 	//main program loop
 	while(1) {
-		m.forward(10, 100);
-		m.backward(10, 100);
+		//get blob positions from camera
+		vector<Point2f> points = c.getKeypoints();
+
+		if(points.size() > 0) {
+			cout << points[0].x << ", " << points[0].y << endl;
+		}
+
+		//get processed image from camera
+		Mat img = c.getFilteredImage();
+
+		//display image
+		imshow("Foosball", img);
+
+		//wait for exit keypress
+		int key = cv::waitKey(1);
+		key = (key==255) ? -1 : key; //#Solve bug in 3.2.0
+		if (key>=0)
+			break;
 	}
 
 	return 0;
